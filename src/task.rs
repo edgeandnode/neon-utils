89ffe9fa49b47f7431d7f46d1ae1edcb7294c873
Pablo Carranza Velez <pablo@edgeandnode.com>
2023-08-12 18:13:50 -0300

    WIP on porting to Neon NAPI backend (not working atm)

--- src/task.rs
+++ src/task.rs
@@ -2,53 +2,31 @@ use crate::errors::{IntoError, Terminal};
 use crate::marshalling::IntoHandle;
 use atomic_take::AtomicTake;
 use neon::prelude::*;
-
-struct TaskWrapper<F> {
-    f: AtomicTake<F>,
-}
-
-impl<F> TaskWrapper<F> {
-    pub fn new(f: F) -> Self {
-        Self {
-            f: AtomicTake::new(f),
-        }
-    }
-}
-
-impl<F, Ok, Err> Task for TaskWrapper<F>
-where
-    F: 'static + Send + FnOnce() -> Result<Ok, Err>,
-    Err: 'static + Send + IntoError,
-    Ok: 'static + Send + IntoHandle,
-    Result<Ok, Err>: Terminal<Handle = Ok::Handle>,
-{
-    type Output = Ok;
-    type Error = Err;
-    type JsEvent = <Ok as IntoHandle>::Handle;
-
-    fn perform(&self) -> Result<Self::Output, Self::Error> {
-        let f = self.f.take().unwrap();
-        f()
-    }
-
-    fn complete(
-        self,
-        cx: TaskContext,
-        result: Result<Self::Output, Self::Error>,
-    ) -> JsResult<Self::JsEvent> {
-        result.finish(cx)
-    }
-}
+use std::sync::Arc;
 
 /// Runs a function asynchronously then calls
 /// the callback with the result.
-pub fn run_async<'c, F, Ok, Err>(callback: Handle<JsFunction>, f: F)
+pub fn run_async<'c, F, Ok, Err>(mut cx: FunctionContext, callback: Handle<JsFunction>, f: F)
 where
     F: 'static + Send + FnOnce() -> Result<Ok, Err>,
     Err: 'static + Send + IntoError,
     Ok: 'static + Send + IntoHandle,
     Result<Ok, Err>: Terminal<Handle = Ok::Handle>,
 {
-    let task = TaskWrapper::new(f);
-    task.schedule(callback);
+    let f_taken = Arc::new(AtomicTake::new(f));
+    let channel = cx.channel();
+    let callback = callback.root(&mut cx);
+    std::thread::spawn(move || {
+        let f_unwrapped = f_taken.take().unwrap();
+        let result = f_unwrapped().map_err(|err| err.into_error(&mut cx));
+        let cb_args = match result {
+            Ok(ok) => vec![cx.null().upcast::<JsValue>()],
+            Err(err) => vec![cx.string(err.to_string()).upcast::<JsValue>()],
+        };
+        channel.send(move |mut _cx| {
+            let callback = callback.into_inner(&mut _cx);
+            let _r: Handle<JsValue> = callback.call_with(&mut _cx).args(cb_args).apply(&mut _cx)?;
+            Ok(())
+        });
+    });
 }
